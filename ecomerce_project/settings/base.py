47e20dc847237a72c26712e129ccf579cf92124e
CarlosPerez <carlosmperez2901@gmail.com>
2024-07-29 13:58:03 -0700

    Added new models such as User and Address

--- ecomerce_project/settings/base.py
+++ ecomerce_project/settings/base.py
@@ -34,7 +34,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'users',
 ]
+
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
