3a4c3b09579badd7be045810208dffdc2109ae78
CarlosPerez <carlosmperez2901@gmail.com>
2024-08-10 17:03:23 -0700

    Refactored some test and models of the users app

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
+""" This file is used to register the models in the admin panel. """
+
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Product, Category
+
+admin.site.register(Product)
+admin.site.register(Category)
--- users/views.py
+++ users/views.py
@@ -1,113 +1,159 @@
+""" This file contains the views for the users app. """
+
 from typing import Iterable
 
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import AnonymousUser, AbstractBaseUser
+from django.contrib.auth import authenticate
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_201_CREATED
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_400_BAD_REQUEST,
+    HTTP_201_CREATED,
+    HTTP_403_FORBIDDEN,
+)
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import AnonymousUser
-from django.contrib.auth import authenticate
 
 from users.models import User, Address
 from users.serializers import UserSerializer, AddressSerializer
 
 
 class SignupView(APIView):
+    """This view is used to create a new user."""
 
-    def post(self,request:Request) -> Response:
+    def post(self, request: Request) -> Response:
+        """Create a new user."""
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=HTTP_201_CREATED)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
+
 class LoginView(APIView):
-    def post(self,request:Request) -> Response:
-        username:str|None = request.data.get('username')
-        password:str|None = request.data.get('password')
-        user:User|None = authenticate(username=username, password=password)
+    """This view is used to authenticate a user."""
+
+    def post(self, request: Request) -> Response:
+        """Authenticate a user."""
+        username: str | None = request.data.get("username")
+        password: str | None = request.data.get("password")
+        user: User | AbstractBaseUser | None = authenticate(
+            username=username, password=password
+        )
 
         if isinstance(user, User):
-            token:Token = get_object_or_404(Token, user=user)
-            return Response({'token': token.key}, status=HTTP_200_OK)
+            token: Token = get_object_or_404(Token, user=user)
+            return Response({"token": token.key}, status=HTTP_200_OK)
 
         return Response(status=HTTP_400_BAD_REQUEST)
 
+
 class UserListView(APIView):
+    """This view is used to get all users."""
 
     permission_classes = [IsAuthenticated]
-    def get(self, request:Request) -> Response:
+
+    def get(self, request: Request) -> Response:
+        """Get all users."""
         print(request.user)
-        users:Iterable = User.objects.all()
-        serializer:UserSerializer = UserSerializer(users, many=True)
+        users: Iterable = User.objects.all()
+        serializer: UserSerializer = UserSerializer(users, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
+
 
 class UserCreateAddressView(APIView):
+    """This view is used to create an address for a user."""
+
     permission_classes = [IsAuthenticated]
 
-    def post(self, request:Request) -> Response:
-        user:User|AnonymousUser = request.user
-        address:Address = Address.objects.create(**request.data)
-        user.address = address 
-        user.save()
-        return Response(status=HTTP_201_CREATED)
+    def post(self, request: Request) -> Response:
+        """Create an address for a user."""
+        user: User | AnonymousUser | AbstractBaseUser = request.user
+        address: Address = Address.objects.create(**request.data)
+        if isinstance(user, User):
+            user.address = address
+            user.save()
+            return Response(status=HTTP_201_CREATED)
+        return Response(status=HTTP_400_BAD_REQUEST)
+
 
 class UserDetailView(APIView):
+    """This view is used to get, update and delete a user."""
+
     permission_classes = [IsAuthenticated]
 
-    def get(self, request:Request, pk:int) -> Response:
-        user:User = get_object_or_404(User, pk=pk)
-        serializer:UserSerializer = UserSerializer(user)
+    def get(self, _request: Request, pk: int) -> Response:
+        """Get a user."""
+        user: User = get_object_or_404(User, pk=pk)
+        serializer: UserSerializer = UserSerializer(user)
         return Response(serializer.data, status=HTTP_200_OK)
 
-    def put(self, request:Request, pk:int) -> Response:
-        user:User = get_object_or_404(User, pk=pk)
-        serializer:UserSerializer = UserSerializer(instance=user, data=request.data)
+    def put(self, request: Request, pk: int) -> Response:
+        """Update a user."""
+        user: User = get_object_or_404(User, pk=pk)
+        serializer: UserSerializer = UserSerializer(instance=user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=HTTP_200_OK)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
-    def delete(self, request:Request, pk:int) -> Response:
-        user:User = get_object_or_404(User, pk=pk)
+    def delete(self, request: Request, pk: int) -> Response:
+        """Delete a user."""
+        if request.user.pk != pk:
+            return Response(status=HTTP_403_FORBIDDEN)
+        user: User = get_object_or_404(User, pk=pk)
         user.delete()
         return Response(status=HTTP_200_OK)
 
+
 class AddressListView(APIView):
+    """This view is used to get all addresses and create a new address."""
+
     permission_classes = [IsAuthenticated]
 
-    def get(self, request:Request) -> Response:
-        addresses:Iterable = Address.objects.all()
-        serializer:AddressSerializer = AddressSerializer(addresses, many=True)
+    def get(self, _request: Request) -> Response:
+        """Get all addresses."""
+        addresses: Iterable = Address.objects.all()
+        serializer: AddressSerializer = AddressSerializer(addresses, many=True)
         return Response(serializer.data, status=HTTP_200_OK)
 
-    def post(self, request:Request) -> Response:
-        serializer:AddressSerializer = AddressSerializer(data=request.data)
+    def post(self, request: Request) -> Response:
+        """Create a new address."""
+        serializer: AddressSerializer = AddressSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=HTTP_201_CREATED)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
+
 class AddressDetailView(APIView):
+    """This view is used to get, update and delete an address."""
+
     permission_classes = [IsAuthenticated]
 
-    def get(self, request:Request, pk:int) -> Response:
-        address:Address = get_object_or_404(Address, pk=pk)
-        serializer:AddressSerializer = AddressSerializer(address)
+    def get(self, _request: Request, pk: int) -> Response:
+        """Get an address."""
+        address: Address = get_object_or_404(Address, pk=pk)
+        serializer: AddressSerializer = AddressSerializer(address)
         return Response(serializer.data, status=HTTP_200_OK)
 
-    def put(self, request:Request, pk:int) -> Response:
-        address:Address = get_object_or_404(Address, pk=pk)
-        serializer:AddressSerializer = AddressSerializer(instance=address, data=request.data)
+    def put(self, request: Request, pk: int) -> Response:
+        """Update an address."""
+        address: Address = get_object_or_404(Address, pk=pk)
+        serializer: AddressSerializer = AddressSerializer(
+            instance=address, data=request.data
+        )
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=HTTP_200_OK)
         return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
-    def delete(self, request:Request, pk:int) -> Response:
-        address:Address = get_object_or_404(Address, pk=pk)
+    def delete(self, _request: Request, pk: int) -> Response:
+        """Delete an address."""
+        address: Address = get_object_or_404(Address, pk=pk)
         address.delete()
         return Response(status=HTTP_200_OK)
